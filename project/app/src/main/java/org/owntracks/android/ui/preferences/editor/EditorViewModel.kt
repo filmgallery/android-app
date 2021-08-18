rsat/android

Migrate editor activity to AndroidViewModel

project/app/src/main/java/org/owntracks/android/ui/preferences/editor/EditorViewModel.kt
@@ -1,34 +1,32 @@
 package org.owntracks.android.ui.preferences.editor
 
-import android.os.Bundle
-import androidx.databinding.Bindable
-import dagger.hilt.android.scopes.ActivityScoped
-import org.owntracks.android.BR
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import org.owntracks.android.R
 import org.owntracks.android.data.repos.WaypointsRepo
 import org.owntracks.android.support.Parser
 import org.owntracks.android.support.Preferences
-import org.owntracks.android.ui.base.viewmodel.BaseViewModel
+import org.owntracks.android.support.preferences.OnModeChangedPreferenceChangedListener
 import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
 
-@ActivityScoped
+@HiltViewModel
 class EditorViewModel @Inject constructor(
     private val preferences: Preferences,
-    private val parser: Parser
-) : BaseViewModel<EditorMvvm.View>(), EditorMvvm.ViewModel<EditorMvvm.View> {
-    @get:Bindable
-    @Bindable
-    override var effectiveConfiguration: String? = null
-        private set
+    private val parser: Parser,
+    private val waypointsRepo: WaypointsRepo
+) : ViewModel(), OnModeChangedPreferenceChangedListener {
 
+    private val mutableConfiguration = MutableLiveData("")
+    val effectiveConfiguration: LiveData<String>
+        get() = mutableConfiguration
 
-    @Inject
-    lateinit var waypointsRepo: WaypointsRepo
-
-    override fun attachView(savedInstanceState: Bundle?, view: EditorMvvm.View) {
-        super.attachView(savedInstanceState, view)
+    init {
+        preferences.registerOnPreferenceChangedListener(this)
         updateEffectiveConfiguration()
     }
 
@@ -37,21 +35,25 @@ class EditorViewModel @Inject constructor(
             val message = preferences.exportToMessage()
             message.waypoints = waypointsRepo.exportToMessage()
             message[preferences.getPreferenceKey(R.string.preferenceKeyPassword)] = "********"
-            setEffectiveConfiguration(parser.toJsonPlainPretty(message))
+            mutableConfiguration.postValue(parser.toJsonPlainPretty(message))
         } catch (e: IOException) {
             Timber.e(e)
-            view?.displayLoadFailed()
+//            view?.displayLoadFailed()
         }
     }
 
-    @Bindable
-    private fun setEffectiveConfiguration(effectiveConfiguration: String) {
-        this.effectiveConfiguration = effectiveConfiguration
-        notifyPropertyChanged(BR.effectiveConfiguration)
+
+    override fun onCleared() {
+        preferences.unregisterOnPreferenceChangedListener(this)
+        super.onCleared()
+
     }
 
-    override fun onPreferencesValueForKeySetSuccessful() {
+    override fun onAttachAfterModeChanged() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         updateEffectiveConfiguration()
-        notifyPropertyChanged(BR.effectiveConfiguration)
     }
 }

filmgallery/android-app

project/app/src/main/java/org/owntracks/android/ui/preferences/editor/EditorViewModel.kt
@@ -1,34 +1,32 @@
 package org.owntracks.android.ui.preferences.editor
 
-import android.os.Bundle
-import androidx.databinding.Bindable
-import dagger.hilt.android.scopes.ActivityScoped
-import org.owntracks.android.BR
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import org.owntracks.android.R
 import org.owntracks.android.data.repos.WaypointsRepo
 import org.owntracks.android.support.Parser
 import org.owntracks.android.support.Preferences
-import org.owntracks.android.ui.base.viewmodel.BaseViewModel
+import org.owntracks.android.support.preferences.OnModeChangedPreferenceChangedListener
 import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
 
-@ActivityScoped
+@HiltViewModel
 class EditorViewModel @Inject constructor(
     private val preferences: Preferences,
-    private val parser: Parser
-) : BaseViewModel<EditorMvvm.View>(), EditorMvvm.ViewModel<EditorMvvm.View> {
-    @get:Bindable
-    @Bindable
-    override var effectiveConfiguration: String? = null
-        private set
+    private val parser: Parser,
+    private val waypointsRepo: WaypointsRepo
+) : ViewModel(), OnModeChangedPreferenceChangedListener {
 
+    private val mutableConfiguration = MutableLiveData("")
+    val effectiveConfiguration: LiveData<String>
+        get() = mutableConfiguration
 
-    @Inject
-    lateinit var waypointsRepo: WaypointsRepo
-
-    override fun attachView(savedInstanceState: Bundle?, view: EditorMvvm.View) {
-        super.attachView(savedInstanceState, view)
+    init {
+        preferences.registerOnPreferenceChangedListener(this)
         updateEffectiveConfiguration()
     }
 
@@ -37,21 +35,25 @@ class EditorViewModel @Inject constructor(
             val message = preferences.exportToMessage()
             message.waypoints = waypointsRepo.exportToMessage()
             message[preferences.getPreferenceKey(R.string.preferenceKeyPassword)] = "********"
-            setEffectiveConfiguration(parser.toUnencryptedJsonPretty(message))
+            mutableConfiguration.postValue(parser.toUnencryptedJsonPretty(message))
         } catch (e: IOException) {
             Timber.e(e)
-            view?.displayLoadFailed()
+//            view?.displayLoadFailed()
         }
     }
 
-    @Bindable
-    private fun setEffectiveConfiguration(effectiveConfiguration: String) {
-        this.effectiveConfiguration = effectiveConfiguration
-        notifyPropertyChanged(BR.effectiveConfiguration)
+
+    override fun onCleared() {
+        preferences.unregisterOnPreferenceChangedListener(this)
+        super.onCleared()
+
     }
 
-    override fun onPreferencesValueForKeySetSuccessful() {
+    override fun onAttachAfterModeChanged() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         updateEffectiveConfiguration()
-        notifyPropertyChanged(BR.effectiveConfiguration)
     }
 }